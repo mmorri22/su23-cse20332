727fd293d86504e50feb518e607ec3dd9a4a03b4
Matthew Morrison <mmorri22@nd.edu>
2023-06-11 19:49:28 -0400

    Homework 0 Fix

---
+++ homeworks/homework0/hello.c
@@ -0,0 +1,14 @@
+/**********************************
+ * Author Name: Prof. Matthew Morrison
+ * Email: matt.morrison@nd.edu
+ *
+ * This file contains the example of Hello, World.
+ * ********************************/
+
+#include <stdio.h>
+
+int main(void){
+
+	fprintf( stdout, "Hello, World\n" );
+	return 0;
+}
