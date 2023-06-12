9d5bfe7b1355864c64c548e55ec74db8f5dfd233
Matthew Morrison <mmorri22@nd.edu>
2023-06-12 01:34:11 -0400

    Reading 03 While Example

---
+++ readings/reading03/while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( void ){
+
+	int the_int = 0;
+
+	while( the_int <= 10 ){
+
+		fprintf( stdout, "the_int at %p = %d %x\n", &the_int, the_int, the_int );
+		++the_int;
+
+	}
+
+	return 0;
+
+}
