f7fba4c2a0e90a6229d049738775d6a5b05da35a
Matthew Morrison <mmorri22@nd.edu>
2023-06-13 00:45:10 -0400

    Homework 2 Initial Push

--- homeworks/homework2/setup.sh
+++ homeworks/homework2/setup.sh
@@ -1,6 +1,7 @@
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob1.c
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob2.c
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob3.c
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob4.c
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob5.c
-wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Makefile
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2//Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob2.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob3.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob4.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob5.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Makefile
+rm setup.sh
