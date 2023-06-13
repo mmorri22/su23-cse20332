94e8eb22065edb095672e06eccc20f5c006208a3
Matthew Morrison <mmorri22@nd.edu>
2023-06-13 00:47:50 -0400

    Homework 2 script fix

--- homeworks/homework2/setup.sh
+++ homeworks/homework2/setup.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2//Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/homework2/Prob1.c
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob2.c
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob3.c
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20232/main/homeworks/homework2/Prob4.c
