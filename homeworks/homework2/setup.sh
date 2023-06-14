5a10de2d9dc3baa022901d223d30bc8ae627df95
Matthew Morrison <mmorri22@nd.edu>
2023-06-14 07:22:19 -0400

    Updates

--- homeworks/homework2/setup.sh
+++ homeworks/homework2/setup.sh
@@ -3,5 +3,4 @@ wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/hom
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/homework2/Prob3.c
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/homework2/Prob4.c
 wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/homework2/Prob5.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/homeworks/homework2/Makefile
 rm setup.sh
