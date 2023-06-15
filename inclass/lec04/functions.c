13d651bb5b0d18b14cd3078f8bd66aefad5c1e9c
Matthew Morrison <mmorri22@nd.edu>
2023-06-15 09:35:52 -0400

    Final Lecture 4 Update

---
+++ inclass/lec04/functions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* Function Declarations */
+
+int main( void ){
+
+	/* Create Variables */
+	/* Note: Only create variables we are using at intermediate steps */
+
+	/* Call if-else function */
+
+	/* Call while function */
+
+	/* Call for function */
+
+	return 0;
+}
+
+/* Function Definitions */
