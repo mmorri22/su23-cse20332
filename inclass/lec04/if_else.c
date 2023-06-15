1a8e7f283aa1b473e186f92615fb121be2d94d3d
Matthew Morrison <mmorri22@nd.edu>
2023-06-15 08:24:14 -0400

    Lecture 04 Update

---
+++ inclass/lec04/if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( void ){
+
+	/* Create variables for comparison */
+
+
+	/* If-Else If Build */
+
+
+	return 0;
+
+}
---
+++ inclass/lec04/switch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( void ){
+
+	/* Create variables for comparison */
+	int switch_int = 3;
+
+	/* If-Else If Build */
+
+
+	return 0;
+
+}
