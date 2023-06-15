714aa3f1d410580dde7489978e4a571510d8f3e8
Matthew Morrison <mmorri22@nd.edu>
2023-06-15 09:47:31 -0400

    Functions Update

--- inclass/lec04/functions.c
+++ inclass/lec04/functions.c
@@ -7,6 +7,8 @@ int main( void ){
 	/* Create Variables */
 	/* Note: Only create variables we are using at intermediate steps */
 
+	/* Average of two doubles */
+
 	/* Call if-else function */
 
 	/* Call while function */
