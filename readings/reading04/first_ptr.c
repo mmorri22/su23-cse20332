9e5b6f2168a19cfc1da51032bf1bdfa8ac887190
Matthew Morrison <mmorri22@nd.edu>
2023-06-17 14:15:21 -0400

    Reading 4 Update

--- readings/reading04/first_ptr.c
+++ readings/reading04/first_ptr.c
@@ -3,13 +3,13 @@
 int main(){
 	
 	int x = 10;
-	int y = 20;
+	int y = -20;
 	
 	int* x_ptr = &x;
 	int* y_ptr = &y;
 	
-	fprintf( stdout, "%d %p %p\n", x, &x, x_ptr );
-	fprintf( stdout, "%d %p %p\n", y, &y, y_ptr );
+	fprintf( stdout, "%d %x %p %p %p\n", x, x, &x, x_ptr, &x_ptr );
+	fprintf( stdout, "%d %x %p %p %p\n", y, y, &y, y_ptr, &y_ptr );
 	
 	return 0;
 }
