109af2883e41e0049e3e1896177e5fe81d80619c
Matthew Morrison <mmorri22@nd.edu>
2023-06-19 09:24:30 -0400

    Lecture 05 Update

--- inclass/lec05/hours.c
+++ inclass/lec05/hours.c
@@ -1,4 +1,4 @@
-/*************************************
+/**************************************
  * File name: insertion_sort.c
  * Initial Author: Prof. Matthew Morrison
  * Modified: [Your Name],[Date]
@@ -6,17 +6,16 @@
  *
  * This function contains the main driver program
  * For the insertion sort in-class coding opportunity
- **************************************/
+ ***************************************/
 
 #include <stdio.h>
 
-/* Step 5 - Include util.h library here when we are doing the Header Files */
+/* Step 6 - Include util.h library here when we are doing the Header Files */
 
 /* Step 1 - Compiler Directive MINUTES */
-#define MINUTES 60
 
 /* Step 3 - Function prototypes */
-void convert_time_to_hm (unsigned int time, unsigned int *p_hours, unsigned int *p_minutes);
+
 
 int main()
 {
@@ -27,13 +26,13 @@ int main()
     fprintf( stdout, "Enter a time duration in minutes: " );
 
     /* Step 2 - First Attempt at fscanf */
-    fscanf ( stdin, "%u", &time );
+
 
     /* Step 2b - Print the result to the terminal */
-    fprintf( stdout, "Number of minutes entered: %u\n", time );
 
-    /* Step X - Call the convert_time_to_hm function with hours and minutes passed by ref */
-    convert_time_to_hm(time, &hours, &minutes);
+
+    /* Step 5 - Call the convert_time_to_hm function with hours and minutes passed by ref */
+
 
     /* Print the result */
     fprintf(stdout, "HH:MM format: %u:%02u\n", hours, minutes);
@@ -44,7 +43,3 @@ int main()
 /* Function Definitions */
 
 /* Step 4 - Write the function definition of convert_time_to_hm */
-void convert_time_to_hm (unsigned int time, unsigned int *p_hours, unsigned int *p_minutes){
-    *p_hours = time / MINUTES;
-    *p_minutes = time % MINUTES;
-}
