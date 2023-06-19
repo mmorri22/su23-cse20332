289f519d66bd7737da74c31b352863b0bb188da2
Matthew Morrison <mmorri22@nd.edu>
2023-06-19 09:16:29 -0400

    Lecture 05 Update

--- inclass/lec05/hours.c
+++ inclass/lec05/hours.c
@@ -10,12 +10,12 @@
 
 #include <stdio.h>
 
-/* Step X - Include util.h library here */
+/* Step 5 - Include util.h library here when we are doing the Header Files */
 
-/* Step 1 - Compiler Directives */
+/* Step 1 - Compiler Directive MINUTES */
 #define MINUTES 60
 
-/* Function prototypes */
+/* Step 3 - Function prototypes */
 void convert_time_to_hm (unsigned int time, unsigned int *p_hours, unsigned int *p_minutes);
 
 int main()
@@ -23,11 +23,14 @@ int main()
     /* Declared Variables */
     unsigned int time, hours, minutes;
 
-    fprintf(stdout, "Test program to convert time values\n");
-    fprintf(stdout, "Enter a time duration in minutes: ");
+    fprintf( stdout, "Test program to convert time values\n" );
+    fprintf( stdout, "Enter a time duration in minutes: " );
 
-    /* Step X - First Attempt at fscanf */
-    fscanf (stdin, "%u", &time);
+    /* Step 2 - First Attempt at fscanf */
+    fscanf ( stdin, "%u", &time );
+
+    /* Step 2b - Print the result to the terminal */
+    fprintf( stdout, "Number of minutes entered: %u\n", time );
 
     /* Step X - Call the convert_time_to_hm function with hours and minutes passed by ref */
     convert_time_to_hm(time, &hours, &minutes);
@@ -40,7 +43,7 @@ int main()
 
 /* Function Definitions */
 
-/* Step X - Write the function definition of convert_time_to_hm */
+/* Step 4 - Write the function definition of convert_time_to_hm */
 void convert_time_to_hm (unsigned int time, unsigned int *p_hours, unsigned int *p_minutes){
     *p_hours = time / MINUTES;
     *p_minutes = time % MINUTES;
