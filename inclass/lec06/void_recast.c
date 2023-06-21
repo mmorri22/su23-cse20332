d5ed4e7eaf5c04b5f087ffa3bf34825f41da583e
Matthew Morrison <mmorri22@nd.edu>
2023-06-21 14:57:51 -0400

    Update

---
+++ inclass/lec06/void_recast.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  /* Step 1a - Allocate a length of four */
+
+
+  /* Step 1b - Allocate a void pointer array */
+
+
+  /* Step 2 - Using pointer arithmetic and ASCII hex, save values A, B, C, D */
+
+
+  /* Step 3 - Loop through and print addresses */
+
+
+  /* Step 4 - Loop through and print addresses */
+
+
+
+  /* Step 5 - Cast the address to int and print the result */
+
+
+  /* Step 1c - Free the void array*/
+  free(void_array);
+
+  return 0;
+}
---
+++ inclass/lec07/void_recast.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  /* Step 1a - Allocate a length of four */
+
+
+  /* Step 1b - Allocate a void pointer array */
+
+
+  /* Step 3 - Using pointer arithmetic and ASCII hex, save values A, B, C, D */
+
+
+  /* Step 4 - Loop through and print addresses */
+
+
+  /* Step 5 - Loop through and print addresses */
+
+
+
+  /* Step 6 - Cast the address to int and print the result */
+
+
+  /* Step 1c - Free the void array*/
+  free(void_array);
+
+  return 0;
+}
