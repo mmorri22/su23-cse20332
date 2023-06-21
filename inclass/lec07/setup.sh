dcf39ff38909297b0be121933fbf5e1d52893211
Matthew Morrison <mmorri22@nd.edu>
2023-06-20 21:37:10 -0400

    Lecture 7 In Class Example

---
+++ inclass/lec07/setup.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/Makefile
+wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/stat_dyn_comp.c
+rm setup.sh
